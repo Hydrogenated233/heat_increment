4da5b9db69335ca799df24e1ad5f5afa0deebc95
Hydrogenated233 <zengfanuchina@gmail.com>
2024-01-06 14:34:13 +0800

    v0.0.0_0.3

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var get = 1
 var t = 0
-buyed = [0,0,0]
+var rebirth_point = 0
+buyed = [0,0,0,0,0]
 up1price = 10;
+function auto_re(){
+    rebirth_point = Math.round(Math.floor(t**0.09) * 0.8) + rebirth_point;
+}
 function auto_click() {
                 addt();
                 update();
@@ -9,6 +13,8 @@ function auto_click() {
 function update(){
     document.getElementById("t").innerHTML = "你面前的这块物体现在有"+ t +"K";
     document.getElementById("get").innerHTML = "增加"+ get +"K";
+    document.getElementById("re").innerHTML = "吸收热量并获得" + Math.floor(t**0.09) + "个科技点";
+    document.getElementById("ret").innerHTML = "你有" + rebirth_point + "科技点"
 }
 function addt(){
     t = t + get;
@@ -16,7 +22,7 @@ function addt(){
     console.log(buyed);
 }
 function up1(){
-    if (buyed[0] < 11){
+    if (buyed[0] < 10){
         if (t > up1price || t == up1price){
             t = t - up1price;
             get = get + 2;
@@ -50,6 +56,38 @@ function up3(){
             document.getElementById("up3").innerHTML="已购买";
             clearInterval(up2e);
             setInterval(auto_click, 500);
+        }
+    }
+    update();
+}
+function re(){
+    rebirth_point = rebirth_point + Math.floor(t**0.09);
+    t = 0;
+    if (buyed[4] == 1){
+        get = get + Math.floor(rebirth_point**0.6+1)
+    }
+    document.getElementById("ret").innerHTML="你有" + rebirth_point + "科技点"
+    update();
+}
+function reup1(){
+    if (buyed[3] == 0){
+        self.price = 1;
+        if (rebirth_point > self.price || rebirth_point == self.price){
+            rebirth_point = rebirth_point - self.price;
+            buyed[3] = 1;
+            document.getElementById("reup1").innerHTML="已购买";
+            reup1e = setInterval(auto_re,1000);
+        }
+    }
+    update();
+}
+function reup2(){
+    if (buyed[4] == 0){
+        self.price = 3;
+        if (rebirth_point > self.price || rebirth_point == self.price){
+            rebirth_point = rebirth_point - self.price;
+            buyed[4] = 1;
+            document.getElementById("reup2").innerHTML="已购买";
         }
     }
     update();
